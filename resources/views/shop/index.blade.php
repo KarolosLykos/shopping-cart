43eac25dd98b0b16ccfddf093df07596dc94ed76
kariolos <l.g.karolos@gmail.com>
2016-11-22 03:21:08 +0200

    Output Data from database

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,86 +5,23 @@
 @stop
 
 @section('content')
-	<div class="row">
-	  <div class="col-sm-6 col-md-4">
-	    <div class="thumbnail">
-	      <img src="http://prodimage.images-bn.com/pimages/9780545139700_p0_v4_s1200x630.jpg" alt="..." class="image-responisve" >
-	      <div class="caption">
-	        <h3>Product Title</h3>
-	        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam velit quod possimus vel accusamus eligendi tenetur, harum doloribus dicta! Molestiae magnam ea veniam saepe vel molestias recusandae accusantium iste nisi!</p>
-	        <div class="clearfix">
-	        	<div class="pull-left price">$12</div>
-	         	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-	     	</div>
-	      </div>
-	    </div>
-	  </div>
-	  <div class="col-sm-6 col-md-4">
-	    <div class="thumbnail">
-	      <img src="http://prodimage.images-bn.com/pimages/9780545139700_p0_v4_s1200x630.jpg" alt="..." class="image-responisve" >
-	      <div class="caption">
-	        <h3>Product Title</h3>
-	        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam velit quod possimus vel accusamus eligendi tenetur, harum doloribus dicta! Molestiae magnam ea veniam saepe vel molestias recusandae accusantium iste nisi!</p>
-	        <div class="clearfix">
-	        	<div class="pull-left price">$12</div>
-	         	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-	     	</div>
-	      </div>
-	    </div>
-	  </div>
-	  <div class="col-sm-6 col-md-4">
-	    <div class="thumbnail">
-	      <img src="http://prodimage.images-bn.com/pimages/9780545139700_p0_v4_s1200x630.jpg" alt="..." class="image-responisve" >
-	      <div class="caption">
-	        <h3>Product Title</h3>
-	        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam velit quod possimus vel accusamus eligendi tenetur, harum doloribus dicta! Molestiae magnam ea veniam saepe vel molestias recusandae accusantium iste nisi!</p>
-	        <div class="clearfix">
-	        	<div class="pull-left price">$12</div>
-	         	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-	     	</div>
-	      </div>
-	    </div>
-	  </div>
-	</div>
-	<div class="row">
-	  <div class="col-sm-6 col-md-4">
-	    <div class="thumbnail">
-	      <img src="http://prodimage.images-bn.com/pimages/9780545139700_p0_v4_s1200x630.jpg" alt="..." class="image-responisve" >
-	      <div class="caption">
-	        <h3>Product Title</h3>
-	        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam velit quod possimus vel accusamus eligendi tenetur, harum doloribus dicta! Molestiae magnam ea veniam saepe vel molestias recusandae accusantium iste nisi!</p>
-	        <div class="clearfix">
-	        	<div class="pull-left price">$12</div>
-	         	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-	     	</div>
-	      </div>
-	    </div>
-	  </div>
-	  <div class="col-sm-6 col-md-4">
-	    <div class="thumbnail">
-	      <img src="http://prodimage.images-bn.com/pimages/9780545139700_p0_v4_s1200x630.jpg" alt="..." class="image-responisve" >
-	      <div class="caption">
-	        <h3>Product Title</h3>
-	        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam velit quod possimus vel accusamus eligendi tenetur, harum doloribus dicta! Molestiae magnam ea veniam saepe vel molestias recusandae accusantium iste nisi!</p>
-	        <div class="clearfix">
-	        	<div class="pull-left price">$12</div>
-	         	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-	     	</div>
-	      </div>
-	    </div>
-	  </div>
-	  <div class="col-sm-6 col-md-4">
-	    <div class="thumbnail">
-	      <img src="http://prodimage.images-bn.com/pimages/9780545139700_p0_v4_s1200x630.jpg" alt="..." class="image-responisve" >
-	      <div class="caption">
-	        <h3>Product Title</h3>
-	        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam velit quod possimus vel accusamus eligendi tenetur, harum doloribus dicta! Molestiae magnam ea veniam saepe vel molestias recusandae accusantium iste nisi!</p>
-	        <div class="clearfix">
-	        	<div class="pull-left price">$12</div>
-	         	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-	     	</div>
-	      </div>
-	    </div>
-	  </div>
-	</div>
+	@foreach ($products->chunk(3) as $productsChunk)
+		<div class="row">
+			@foreach ($productsChunk as $product)
+				<div class="col-sm-6 col-md-4">
+					<div class="thumbnail">
+					  <img src="{{ $product->imagePath }}" alt="..." class="image-responisve" >
+					  <div class="caption">
+					    <h3>{{ $product->title }}</h3>
+					    <p class="description">{{ $product->description }}</p>
+					    <div class="clearfix">
+					    	<div class="pull-left price">$ {{ $product->price }}</div>
+					     	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+					 	</div>
+					  </div>
+					</div>
+				</div>
+			@endforeach
+		</div>
+	@endforeach
 @stop
