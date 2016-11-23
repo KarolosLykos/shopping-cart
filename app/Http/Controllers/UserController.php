6b482f1283e7b58c4dd40160467afc9a0abf0522
kariolos <l.g.karolos@gmail.com>
2016-11-23 20:43:42 +0200

    Added Middleware

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,37 +1,34 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\User;
-use Auth;
 use Illuminate\Http\Request;
-
+use Auth;
 class UserController extends Controller
 {
-    public function getSignup() {
-    	return view('user.signup');
+    public function getSignup()
+    {
+        return view('user.signup');
     }
+    public function postSignup(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'email|required|unique:users',
+            'password' => 'required|min:4'
+        ]);
+        $user = new User([
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password'))
+        ]);
+        $user->save();
 
-    public function postSignup(Request $request) {
-    	$this->validate($request, [
-    		'email'=> 'email|required|unique:users',
-    		'password'=> 'required|min:4'
-    		]);
-
-    	$user = new User([
-    		'email'=> $request->input('email'),
-    		'password'=> bcrypt($request->input('password'))
-    	]);
-
-    	$user->save();
-
-    	return redirect()->route('product.index');
+        Auth::login($user);
+        
+        return redirect()->route('user.profile');
     }
-
-    public function getSignin() {
+    public function getSignin()
+    {
         return view('user.signin');
     }
-
     public function postSignin(Request $request)
     {
         $this->validate($request, [
@@ -43,8 +40,12 @@ class UserController extends Controller
         }
         return redirect()->back();
     }
-
-    public function getProfile(){
+    public function getProfile() {
         return view('user.profile');
+    }
+    
+    public function getLogout() {
+        Auth::logout();
+        return redirect()->back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,46 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Application Routes
 |--------------------------------------------------------------------------
 |
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
 |
 */
 
-Route::get('/', ['uses' => 'ProductController@getIndex','as'   => 'product.index']);
-
-
-Route::get('/signup', ['uses' => 'UserController@getSignup', 'as' => 'user.signup']);
-
-Route::post('/signup', ['uses' => 'UserController@postSignup', 'as' => 'user.signup']);
-
-
-Route::get('/signin', ['uses' => 'UserController@getSignin', 'as' => 'user.signin']);
-
-Route::post('/signin', ['uses' => 'UserController@postSignin', 'as' => 'user.signin']);
-
-
-Route::get('/user/profile',['uses'=> 'UserController@getProfile', 'as' => 'user.profile']);
+Route::get('/', [
+    'uses' => 'ProductController@getIndex',
+    'as' => 'product.index'
+]);
+Route::group(['prefix' => 'user'], function() {
+    Route::group(['middleware' => 'guest'], function() {
+        Route::get('/signup', [
+            'uses' => 'UserController@getSignup',
+            'as' => 'user.signup'
+        ]);
+        Route::post('/signup', [
+            'uses' => 'UserController@postSignup',
+            'as' => 'user.signup'
+        ]);
+        Route::get('/signin', [
+            'uses' => 'UserController@getSignin',
+            'as' => 'user.signin'
+        ]);
+        Route::post('/signin', [
+            'uses' => 'UserController@postSignin',
+            'as' => 'user.signin'
+        ]);
+    });
+    Route::group(['middleware' => 'auth'], function() {
+        Route::get('/profile', [
+            'uses' => 'UserController@getProfile',
+            'as' => 'user.profile'
+        ]);
+        Route::get('/logout', [
+            'uses' => 'UserController@getLogout',
+            'as' => 'user.logout'
+        ]);
+    });
+});
