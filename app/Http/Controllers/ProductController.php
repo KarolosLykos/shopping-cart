f4fd8a1ed0b6ebd0a628c613e6987676d9862d3f
kariolos <l.g.karolos@gmail.com>
2016-11-24 18:35:36 +0200

    Added orders model/migration

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Http\Controllers;
 use App\Cart;
+use Auth;
 use App\Product;
+use App\Order;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use Session;
@@ -51,15 +53,23 @@ class ProductController extends Controller
         $cart = new Cart($oldCart);
         Stripe::setApiKey('sk_test_WHy8JS7TnzW1oxaoW7GQxdRL');
         try {
-            Charge::create(array(
+           $charge = Charge::create(array(
                 "amount" => $cart->totalPrice * 100,
                 "currency" => "usd",
                 "source" => $request->input('stripeToken'), // obtained with Stripe.js
                 "description" => "Test Charge"
             ));
+           $order = new Order();
+           $order->cart = serialize($cart);
+           $order->address = $request->input('address');
+           $order->name = $request->input('name');
+           $order->payment_id = $charge->id;
+
+           Auth::user()->orders()->save($order);
         } catch (\Exception $e) {
             return redirect()->route('checkout')->with('error', $e->getMessage());
         }
+
         Session::forget('cart');
         return redirect()->route('product.index')->with('success', 'Successfully purchased products!');
     }
